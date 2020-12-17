df328060afaa639185916b54c305477a4b4d233d
siegelion <1448189829@qq.com>
2020-12-17 11:34:58 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from scapy import *
+from scapy.layers.l2 import ARP, Ether
+from scapy.sendrecv import srp
+
+IpScan = '192.168.1.0/24'
+try:
+    ans, unans = srp(Ether(dst="FF:FF:FF:FF:FF:FF") / ARP(pdst=IpScan), timeout=2)
+except Exception as e:
+    print(e)
+else:
+    for send, rcv in ans:
+        ListMACAddr = rcv.sprintf("%Ether.src%---%ARP.psrc%")
+        print(ListMACAddr)
